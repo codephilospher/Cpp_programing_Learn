473ba7818911ba55de5a2148caecae2467abba80
code philospher <sharmashivraj506@gmail.com>
2026-07-22 23:19:00 +0530

    create basic SUM of two numbers

--- FIrst_program.cpp
+++ FIrst_program.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    int NUM1, NUM2, SUM;
+    cout << "Enter first number: ";
+    cin>>NUM1;
+    cout << "Enter second number: ";
+    cin>>NUM2;
+    SUM = NUM1 + NUM2;
+    cout << "Sum of " << NUM1 << " and " << NUM2 << " is: " << SUM << endl;
+    
     return 0;
 }
