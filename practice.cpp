7b047a31526dd368eeba811a3b4760db3683c5b2
code philospher <sharmashivraj506@gmail.com>
2026-07-28 16:41:05 +0530

    striver pattern no 18 solved on print11

--- practice.cpp
+++ practice.cpp
@@ -173,8 +173,24 @@ void print11(int n){
         cout << endl;
     }
 }
+void print12(int n){
+   
+    
+    for(int i=0; i<n; i++){
+        for( char ch='E'-i; ch<='E'; ch++){
+            cout << ch << " ";
+           
+           
+            
+        }
+        
+        
+        cout << endl;
+       
 
+}
 
+}
 int main() {
     int n;
     cout << "Enter a number: ";
@@ -182,7 +198,7 @@ int main() {
     for (int i = 1; i <= n; i++) {
        int t;
        cin >> t;
-       print11(t);
+       print12(t);
     }
    
     return 0;
