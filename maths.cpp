dd079dbcff7739aaf1bd51394158dce3abbe9f42
code philospher <sharmashivraj506@gmail.com>
2026-08-02 15:08:26 +0530

    maths menu created

---
+++ maths.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int choice;
+    while (true)
+    {
+         cout<<"\n=========Basic Maths Menu==========\n"<<endl;
+   
+    cout<<"1. Count all digits in a number"<<endl;
+    cout<<"2. Reverse a numbern"<<endl;
+    cout<<"3. Check if a number is palindrome"<<endl;
+    cout<<"4. GCD of two numbers"<<endl;
+    cout<<"5. check if a number is Armstrong or not"<<endl;
+    cout<<"6. Print all Divisors"<<endl;
+    cout<<"7. check for a prime number"<<endl;
+    cout<<"8. Exit"<<endl;
+     cout<<"===================================="<<endl;
+     cout<<"Enter your choice: ";
+    cin>>choice;
+    return 0;
+
+        
+    }
+    
+   
+
+
+    return 0;
+}
