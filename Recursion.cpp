b74ee7f3704bca3aa82d18fd1f9fd3f7d1c14356
code philospher <sharmashivraj506@gmail.com>
2026-08-06 23:04:50 +0530

    factorial by recurions

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,18 +1,88 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int factorial(int fact,int n)
-{
-    if(n == 0){
-    return fact;
-    }
 
-     factorial(fact*n,n-1);
+int factorial( int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
 }
 
-int main()
-{
-    int n;
-    cin >> n;
-    cout << "Factorial of " << n << " is " <<factorial(1,n) << endl;
+void reversearray(int arr[], int n, int i) {
+    if (i >= n / 2) {
+        return;
+    }
+    swap(arr[i], arr[n - i - 1]);
+    reversearray(arr, n, i + 1);
+}
+
+void printArray(const int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void showMenu() {
+    cout << "\n===== Recursion Menu =====" << endl;
+    cout << "1. Find factorial" << endl;
+    cout << "2. Reverse an array" << endl;
+    cout << "3. Add future function here" << endl;
+    cout << "4. Exit" << endl;
+    cout << "Enter your choice: ";
+}
+
+void futureFunction() {
+    cout << "You can add another recursion-based function here later." << endl;
+}
+
+int main() {
+    int choice;
+
+    do {
+        showMenu();
+        cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            int n;
+            cout << "Enter a number: ";
+            cin >> n;
+            cout << "Factorial of " << n << " is " << factorial(n) << endl;
+            break;
+        }
+        case 2: {
+            int n;
+            cout << "Enter array size: ";
+            cin >> n;
+
+            int arr[100];
+            cout << "Enter " << n << " elements: ";
+            for (int i = 0; i < n; i++) {
+                cin >> arr[i];
+            }
+
+            cout << "Original array: ";
+            printArray(arr, n);
+
+            reversearray(arr, n, 0);
+            cout << "Reversed array: ";
+            printArray(arr, n);
+            break;
+        }
+        case 3: {
+            futureFunction();
+            break;
+        }
+        case 4: {
+            cout << "Exiting program..." << endl;
+            break;
+        }
+        default:
+            cout << "Invalid choice! Please try again." << endl;
+            break;
+        }
+    } while (choice != 4);
+
     return 0;
 }
